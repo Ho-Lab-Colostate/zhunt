ae13e0e9c4bb8dfa4db267e936892f12d6686f4e
ryansczarny <63801323+ryansczarny@users.noreply.github.com>
2020-04-20 10:21:14 -0600

    Update zhunt.py

--- scripts/zhunt.py
+++ scripts/zhunt.py
@@ -1,5 +1,5 @@
 import shlex, subprocess
-command_line = "zhunt pBR322.txt"
+command_line = "zhunt 12 6 12 pBR322.fasta"
 args = shlex.split(command_line)
 p = subprocess.Popen(args)
 tmp=subprocess.call("./a.out")
